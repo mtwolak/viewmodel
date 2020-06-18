9e2fc5ece0d8bd588477a54d511d0c4ebb7cd48e
Marcin Wolak <mtwolak@gmail.com>
2020-06-18 15:15:17 +0200

    Working version

--- app/src/main/java/com/example/viewmodel/ui/main/MainViewModel.kt
+++ app/src/main/java/com/example/viewmodel/ui/main/MainViewModel.kt
@@ -6,17 +6,11 @@ import androidx.lifecycle.ViewModel
 
 class MainViewModel : ViewModel() {
     private val testData: MutableLiveData<String> by lazy {
-        MutableLiveData<String>().also {
-            initTestData()
-        }
+        MutableLiveData<String>("initTestData")
     }
 
     fun getTestData(): LiveData<String> {
         return testData
-    }
-
-    private fun initTestData(): String {
-        return "initTestData"
     }
 
 }
